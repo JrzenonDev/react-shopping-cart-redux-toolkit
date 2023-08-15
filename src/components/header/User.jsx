784bdd9e2caac503fd9a4e950b7d71ecd5e1bf59
JrzenonDev <jribertoonb@gmail.com>
2023-08-15 18:58:45 -0300

    feat: create layout component User

--- src/components/header/User.jsx
+++ src/components/header/User.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react";
+import UserAvatar from "../../assets/images/user.png";
+import { IoSettingsOutline } from "react-icons/io5";
+import { BsBagCheck } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+import { GrHelp } from "react-icons/gr";
+import { BiLogOut } from "react-icons/bi";
+
 export const User = () => {
+  const user = true;
+  const [profileOpen, setProfileOpen] = useState(false);
+
+  const close = () => {
+    setProfileOpen(null);
+  };
   return (
     <>
-      <h1>User</h1>
+      <div className="profile">
+        {user ? (
+          <>
+            <button
+              className="img"
+              onClick={() => setProfileOpen(!profileOpen)}
+            >
+              <img src={UserAvatar} alt="User avatar" />
+            </button>
+            {profileOpen && (
+              <div className="openProfile boxItems" onClick={close}>
+                <div className="image">
+                  <div className="img">
+                    <img src={UserAvatar} alt="User avatar" />
+                  </div>
+                  <div className="text">
+                    <h4>Eden Smith</h4>
+                    <label htmlFor="">Los Angeles, CA</label>
+                  </div>
+                </div>
+                <button className="box">
+                  <IoSettingsOutline className="icon" />
+                  <h4>My Account</h4>
+                </button>
+                <button className="box">
+                  <BsBagCheck className="icon" />
+                  <h4>My Order</h4>
+                </button>
+                <button className="box">
+                  <AiOutlineHeart className="icon" />
+                  <h4>WishList</h4>
+                </button>
+                <button className="box">
+                  <GrHelp className="icon" />
+                  <h4>Help</h4>
+                </button>
+                <button className="box">
+                  <BiLogOut className="icon" />
+                  <h4>Log Out</h4>
+                </button>
+              </div>
+            )}
+          </>
+        ) : (
+          <button>My Account</button>
+        )}
+      </div>
     </>
   );
 };
