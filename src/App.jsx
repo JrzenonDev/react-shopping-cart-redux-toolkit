9ce2e85984476d705f46435d9d33f7e4dec4cf39
JrzenonDev <jribertoonb@gmail.com>
2023-08-15 02:32:56 -0300

    feat: configure router in aplication

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Header } from "./components/header/Header";
+import { Home } from "./pages/home/Home";
 
 function App() {
   return (
     <>
-      <main>
-        <h1>Hello</h1>
-      </main>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+        </Switch>
+      </Router>
     </>
   );
 }
