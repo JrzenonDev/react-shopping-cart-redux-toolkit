8ccd488be220e9f5553e543938f2b5f05bfeadca
JrzenonDev <jribertoonb@gmail.com>
2023-08-15 18:15:35 -0300

    feat: loop in product and pass props to component CarItems

--- src/components/header/Card.jsx
+++ src/components/header/Card.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { BiShoppingBag } from "react-icons/bi";
+import { product } from "../../assets/data/data";
+import { CardItems } from "./CardItems";
 
 export const Card = () => {
   const [cardOpen, setCardOpen] = useState(false);
@@ -19,6 +21,24 @@ export const Card = () => {
           <h2>Shopping Cart</h2>
           <button onClick={closeCard}>
             <AiOutlineClose className="icon" />
+          </button>
+        </div>
+        {product.slice(0, 2).map((item) => (
+          <CardItems
+            key={item.id}
+            id={item.id}
+            cover={item.cover}
+            name={item.name}
+            price={item.price}
+            quantity={item.quantity}
+            totalPrice={item.totalPrice}
+          />
+        ))}
+
+        <div className="checkOut">
+          <button>
+            <span>Priceed To Checkout</span>
+            <label>$ 240</label>
           </button>
         </div>
       </div>
