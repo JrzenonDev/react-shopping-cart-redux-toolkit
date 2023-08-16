c4cf3fb8ecefa3cc5c91a6146c4d85a7d2ad682b
JrzenonDev <jribertoonb@gmail.com>
2023-08-16 12:59:01 -0300

    feat: adding component Product in Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import { Category } from "../../components/category/Category";
 import { Order } from "../../components/hero/Order";
 import { Slider } from "../../components/hero/Slider";
+import { Product } from "../../components/product/Product";
 
 export const Home = () => {
   return (
@@ -8,6 +9,7 @@ export const Home = () => {
       <Slider />
       <Order />
       <Category />
+      <Product />
     </>
   );
 };
