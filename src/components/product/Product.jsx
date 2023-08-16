b9be1cd3f46c745be1a31b4d48728b0f959bd8e1
JrzenonDev <jribertoonb@gmail.com>
2023-08-16 12:57:39 -0300

    feat: create component Product

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,22 @@
+import { product } from "../../assets/data/data";
+import { ProductCart } from "./ProductCart";
+
+export const Product = () => {
+  return (
+    <>
+      <section className="product">
+        <div className="container grid3">
+          {product.map((item) => (
+            <ProductCart
+              key={item.id}
+              id={item.id}
+              cover={item.cover}
+              name={item.name}
+              price={item.price}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
