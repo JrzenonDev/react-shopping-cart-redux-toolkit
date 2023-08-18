1f1e4e9f17e317baf11f0767f12094eac5f8d9bd
JrzenonDev <jribertoonb@gmail.com>
2023-08-17 22:08:14 -0300

    feat: create component Register

---
+++ src/pages/login/Register.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import "./login.css";
+import back from "../../assets/images/my-account.jpg";
+
+export const Register = () => {
+  return (
+    <>
+      <section className="login">
+        <div className="container">
+          <div className="backImg">
+            <img src={back} alt="My Account" />
+            <div className="text">
+              <h3>Register</h3>
+              <h1>My Account</h1>
+            </div>
+          </div>
+
+          <form>
+            <span>Email address</span>
+            <input type="text" name="" id="" required />
+            <span>Username *</span>
+            <input type="text" name="" id="" required />
+            <span>Password *</span>
+            <input type="text" name="" id="" required />
+            <span>Confirm Password *</span>
+            <input type="text" name="" id="" required />
+            <button className="button">Register</button>
+            <p>teste</p>
+            <Link to="/login">SignIn</Link>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
