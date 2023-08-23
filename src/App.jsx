06662ee27c45166e6c011ad7c981481c56fcf39d
JrzenonDev <jrobertoonb@gmail.com>
2023-08-23 18:40:03 -0300

    feat: route authentication

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,26 @@ import { Home } from "./pages/home/Home";
 import { Register } from "./pages/login/Register";
 import { Login } from "./pages/login/Login";
 import { Account } from "./pages/account/Account";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isLoggIn = useSelector((state) => state.auth.isLoggIn);
+
   return (
     <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path="/register" component={Register}></Route>
-          <Route exact path="/login" component={Login}></Route>
-          <Route exact path="/account" component={Account}></Route>
-        </Switch>
-        <Footer />
-      </Router>
+      {isLoggIn && (
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/register" component={Register}></Route>
+
+            <Route exact path="/account" component={Account}></Route>
+          </Switch>
+          <Footer />
+        </Router>
+      )}
+      {!isLoggIn && <Login />}
     </>
   );
 }
