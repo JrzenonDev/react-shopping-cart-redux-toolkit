17b3ed14b29a2828a903c0310b6680ffdf8f2ebc
JrzenonDev <jrobertoonb@gmail.com>
2023-08-23 00:44:57 -0300

    feat: get state totalQuantity and itemsList in Card

--- src/components/header/Card.jsx
+++ src/components/header/Card.jsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { BiShoppingBag } from "react-icons/bi";
-import { product } from "../../assets/data/data";
 import { CardItems } from "./CardItems";
+import { useSelector } from "react-redux";
 
 export const Card = () => {
   const [cardOpen, setCardOpen] = useState(false);
   const closeCard = () => {
     setCardOpen(null);
   };
+
+  const quantity = useSelector((state) => state.cart.totalQuantity);
+  const cartItems = useSelector((state) => state.cart.itemsList);
   return (
     <>
       <div className="card" onClick={() => setCardOpen(!cardOpen)}>
         <BiShoppingBag className="cardIcon" />
-        <span className="flexCenter">2</span>
+        <span className="flexCenter">{quantity}</span>
       </div>
       <div className={cardOpen ? "overlay" : "nonoverlay"}></div>
       <div className={cardOpen ? "cartItem" : "cardhide"}>
@@ -23,7 +26,7 @@ export const Card = () => {
             <AiOutlineClose className="icon" />
           </button>
         </div>
-        {product.slice(0, 2).map((item) => (
+        {cartItems.map((item) => (
           <CardItems
             key={item.id}
             id={item.id}
