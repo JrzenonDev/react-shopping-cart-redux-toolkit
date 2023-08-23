52420eb22fa4f6b2743908ce9b2fb884e80d9f34
JrzenonDev <jrobertoonb@gmail.com>
2023-08-23 18:13:25 -0300

    feat: create reducer removeFromCart

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -10,7 +10,7 @@ const cartSlice = createSlice({
     addToCart(state, action) {
       const newItem = action.payload;
 
-      // check item is already exits
+      //check item is already exits
       const exitsItem = state.itemsList.find((item) => item.id === newItem.id);
 
       if (exitsItem) {
@@ -26,6 +26,17 @@ const cartSlice = createSlice({
           cover: newItem.cover,
         });
         state.totalQuantity++;
+      }
+    },
+    removeFromCart(state, action) {
+      const id = action.payload;
+      const exitstingItem = state.itemsList.find((item) => item.id === id);
+      if (exitstingItem.quantity === 1) {
+        state.itemsList = state.itemsList.filter((item) => item.id !== id);
+        state.totalQuantity--;
+      } else {
+        exitstingItem.quantity--;
+        exitstingItem.totalPrice -= exitstingItem.price;
       }
     },
   },
